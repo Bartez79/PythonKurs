a73af79d5bda6d61bd327d0c8fa0414b49f285e6
Michal <michal_kruczkowski@o2.pl>
2017-05-16 13:51:27 +0200

    Podstawy obiektowosci

--- Dziedziczenie.py
+++ Dziedziczenie.py
@@ -19,3 +19,32 @@ class D(B,C):
         
 d = D()
 d.foo()
+
+
+class A1:
+    a=1
+    b=2
+    def __init__(self, a):
+        self.a = a
+    
+    def zwroc(self):   
+        return 'jestem w A1'
+    def dodawanie(self):
+        return self.a + self.a
+class B1(A1):
+    def __init__(self, a, b):
+        super(B1,self)
+        self.b = b
+        print(self.a + self.b)
+        
+    def zwroc(self):
+        return super(B1,self).zwroc() + 'jestem w B1'
+
+    def dodawanie(self, x):
+        super(B1,self)
+        self.x = x
+        return self.a + self.x
+o1 = B1(1,4)
+print(o1.zwroc())
+print(o1.dodawanie(6))
+
